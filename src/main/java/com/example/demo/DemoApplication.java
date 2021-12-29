f8a2bc027f918505fec63b2271cc88fca1d58a59
gty <1115428019@qq.com>
2021-12-29 17:11:22 +0800

    second

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,22 +2,23 @@ package com.example.demo;
 
 import com.example.demo.dao.UserDao;
 import com.example.demo.factory.UserFactory;
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
     public void addUser(){
         System.out.println("Demo.addUser");
-        UserDao userDao = UserFactory.getUserDao();
+        UserDao userDao = null;
+        try {
+            userDao = UserFactory.getUserDao();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         userDao.addUser();
     }
     public static void main(String[] args) {
         new DemoApplication().addUser();
-        //ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
-        //context.getBean();
         SpringApplication.run(DemoApplication.class, args);
     }
 
--- src/main/java/com/example/demo/factory/UserFactory.java
+++ src/main/java/com/example/demo/factory/UserFactory.java
@@ -2,9 +2,20 @@ package com.example.demo.factory;
 
 import com.example.demo.dao.UserDao;
 
+import static java.lang.Class.forName;
+
 public class UserFactory {
-    public  static UserDao getUserDao(){
-        return new UserDao();
+    public  static UserDao getUserDao() throws ClassNotFoundException {
+        String ljy_s_bdz = "com.example.demo.dao.UserDao";
+        Class test = forName(ljy_s_bdz);
+        try {
+            return (UserDao)test.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
