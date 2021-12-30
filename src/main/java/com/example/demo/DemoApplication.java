b12dbf0a84f41b58d5806dceeb417e5a775a5c10
gty <1115428019@qq.com>
2021-12-30 16:36:13 +0800

    second

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,15 @@ package com.example.demo;
 import com.example.demo.dao.UserDao;
 import com.example.demo.dao.test_New;
 import com.example.demo.factory.UserFactory;
+import com.example.demo.test.inF;
+import com.example.demo.test.rel_Inf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -20,8 +27,27 @@ public class DemoApplication {
     }
     public static void main(String[] args) {
 //        new DemoApplication().addUser();
-        new DemoApplication().addTest();
+//        new DemoApplication().addTest();
+        Class [] nsz = {inF.class};
+        rel_Inf nb = new rel_Inf();
+        inF nbb = (inF) Proxy.newProxyInstance(DemoApplication.class.getClassLoader(),nsz,new rel_Proxy(nb));
+        int result = nbb.add(1,2);
+        System.out.println("result:"+result);
         SpringApplication.run(DemoApplication.class, args);
     }
 
+}
+
+class rel_Proxy implements InvocationHandler{
+    private Object obj;
+    public rel_Proxy(Object obj){
+        this.obj=obj;
+    }
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("before:"+method.getName()+"传递的参数"+ Arrays.toString(args));
+        Object res=method.invoke(obj,args);
+        System.out.println("after:"+obj);
+        return res;
+    }
 }
---
+++ src/main/java/com/example/demo/test/inF.java
@@ -0,0 +1,6 @@
+package com.example.demo.test;
+
+public interface inF {
+    public int add(int a,int b);
+    public String update(String id);
+}
