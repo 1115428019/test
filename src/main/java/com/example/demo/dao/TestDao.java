e4ba5b5d1bae3baac6a6769e83a3b1120e173515
gty <1115428019@qq.com>
2022-01-07 10:02:46 +0800

    second

--- src/main/java/com/example/demo/dao/TestDao.java
+++ src/main/java/com/example/demo/dao/TestDao.java
@@ -1,4 +1,12 @@
 package com.example.demo.dao;
 
+
+import com.example.demo.model.TestModel;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface TestDao {
+    @Select("Select * from testtable where id=#{id}")
+    public TestModel getById(long id);
 }
---
+++ src/main/java/com/example/demo/model/TestModel.java
@@ -0,0 +1,43 @@
+package com.example.demo.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestModel {
+    private int weight;
+    private int height;
+    private String name;
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
