c7e6371dd05955ab6ad5122da3c6b4508a3ee33e
gty <1115428019@qq.com>
2022-01-07 17:41:12 +0800

    new

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -4,6 +4,7 @@ import com.example.demo.model.TestModel;
 import com.example.demo.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 //@ResponseBody
@@ -33,11 +34,38 @@ public class HelloWorld {
     return "test";
     }
 
+    @RequestMapping("/url")
+    public String showUrl(Model model){
+        model.addAttribute("id",1);
+        model.addAttribute("age",18);
+        return "url";
+    }
+
     @PostMapping("/test")
     @ResponseBody
     public String delete(int id){
         testService.deleteTestData(id);
         return "delete successfully";
+    }
+
+    @RequestMapping("/show")
+    public String showAll(Model model){
+        TestModel testModel;
+        testModel=testService.getById(0);
+        model.addAttribute("testModel",testModel);
+        return "all";
+    }
+
+    @RequestMapping("/parameter")
+    @ResponseBody
+    public String parameter(String username){
+        return username;
+    }
+
+    @RequestMapping("/multipa")
+    @ResponseBody
+    public String  multi(String name,int age){
+        return name+":"+age;
     }
 
 }
